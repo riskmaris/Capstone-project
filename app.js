a01f9a9a21aa922c533bfda7b745fb98ce93e182
riskmaris <mariskaps20@gmail.com>
2023-04-27 21:08:16 +0300

    make changes

--- app.js
+++ app.js
@@ -1,76 +1,3 @@
-const speakers = document.querySelector('.featured-speakers');
-
-const speakersDetails = [
-  {
-    id: 1,
-    name: 'Mr.Aggrey Kapuka',
-    title: 'C.E.O and founder of Karis Uganda Safaris',
-    speakerImg: 'images/JAV_2396.jpg',
-    about:
-      'Planning, Organizing, Hotel Bookings and Overall',
-  },
-  {
-    id: 2,
-    name: 'Mrs Angella Mubangizi.',
-    title: 'Proffesor | Kampala International University',
-    speakerImg: 'images/Dr.-Winifred-Kiryabwire-1.jpg',
-    about:
-      'KIU Business Desk: Sylvia Mulinge Appointed new CEO of MTN Uganda, Uganda.',
-  },
-  {
-    id: 3,
-    name: 'Mrs Lilly Ajarova',
-    title: ' Best Uganda Safari Tour.',
-    speakerImg: 'images/image-6.png',
-    about:
-      'CEO for Uganda Tourism Board | Responsible Tourism Company.',
-  },
-  {
-    id: 4,
-    name: 'Maris Kapuka',
-    title: 'Managing Director.',
-    speakerImg: 'images/JAV_2500.jpg',
-    about:
-      'The Shareholders, Board of Directors, Management and Staff of Karis Uganda Safaris | Uganda Limited.',
-  },
-  {
-    id: 5,
-    name: 'Mr.Lesh Mugabi',
-    title: 'Senior Analyst on Covid-19',
-    speakerImg: 'images/MD-MM.jpg',
-    about:
-      "The Impact of Covid-19 on Uganda's Tourism Sector, Highlights, Uganda Canada Business Online Expo.",
-  },
-  {
-    id: 6,
-    name: 'Mr Rick Otieno',
-    title: 'Driver',
-    speakerImg: 'images/JAV_2396.jpg',
-    about:
-      'The Head of driving | Car maintaince and garage expert',
-  },
-];
-
-function randomSpeaker() {
-  speakers.innerHTML = speakersDetails
-    .map(
-      (speaker) => `
-         <div class="speakers-details">
-            <img src=${speaker.speakerImg} alt=""/>
-            <div class="details">
-              <h4>${speaker.name}</h4>
-              <p class="title">${speaker.title}</p>
-              <hr/>
-              <p class="position">${speaker.about}</p>            
-          </div>
-          </div>
-`,
-    )
-    .join('');
-}
-
-randomSpeaker();
-
 // ......................Interactions...................
 
 const hamburger = document.querySelector('.hamburger');
@@ -82,6 +9,7 @@ function mobileMenu() {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
+  document.getElementById('border').style.zIndex='-10'
 }
 
 
@@ -92,7 +20,7 @@ function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   document.body.classList.remove('no-scroll');
+  document.getElementById('border').style.zIndex='2'
 }
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
-
