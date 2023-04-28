23e9d802f2b02a9030fa01b84b8a7b17c3a1dd78
riskmaris <mariskaps20@gmail.com>
2023-04-28 09:37:12 +0300

    fix linters

--- app.js
+++ app.js
@@ -4,23 +4,20 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navLink = document.querySelectorAll('.nav-link');
 
-
 function mobileMenu() {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
-  document.getElementById('border').style.zIndex='-10'
+  document.getElementById('border').style.zIndex = '-10';
 }
 
-
 hamburger.addEventListener('click', mobileMenu);
-
 
 function closeMenu() {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   document.body.classList.remove('no-scroll');
-  document.getElementById('border').style.zIndex='2'
+  document.getElementById('border').style.zIndex = '2';
 }
 
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
